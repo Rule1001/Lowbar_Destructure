5b0d3750b1de5d837f7cb9bca9353dfc0ee022e4
Rule1001 <hello@rule1001.com>
2017-09-20 13:50:58 +0100

    reduce function added and tests passing

--- spec/reduce.spec.js
+++ spec/reduce.spec.js
@@ -13,12 +13,12 @@ describe('#reduce', () => {
       expect(actual).to.equal(expected);
       expect(_.reduce({ one: 1, two: 2 }, (acc, val) => acc + val)).to.equal(3);
     });
-    it('should return a single value with and without memo', () => {
+    it('will return a single value', () => {
       expect(_.reduce([1, 2, 3, 4, 5], (acc, num) => acc + num, 0)).to.equal(15);
       expect(_.reduce([1, 2, 3, 4, 5], (acc, num) => acc + num)).to.equal(15);
       expect(_.reduce([0, 1, 2, 3, 4, 5], (acc, num) => acc + num, 0)).to.equal(15);
     });
-    it('should bind the iteratee to a context object if one is passed', () => {
+    it('will bind the iteratee to a context object if one is passed', () => {
       const context = { type: 'house' };
       const result = [];
       const iteratee = () => {
